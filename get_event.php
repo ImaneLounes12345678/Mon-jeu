acdf075df467f25d3c9b44f16205e22364b12fbb
ImaneLounes12345678 <imanelounes351@gmail.com>
2026-04-21 22:46:51 +0200

    Remove event types from event data

--- get_event.php
+++ get_event.php
@@ -2,14 +2,14 @@
 header('Content-Type: application/json');
 
 $evenements = [
-    ["titre" => "Construction de Gizeh", "date" => -2560, "type" => "reel"],
-    ["titre" => "Invasion Alien en Grèce", "date" => -400, "type" => "fiction"],
-    ["titre" => "Sacre de Charlemagne", "date" => 800, "type" => "reel"],
-    ["titre" => "Premier pas sur la Lune", "date" => 1969, "type" => "reel"],
-    ["titre" => "Chute de l'Atlantide", "date" => -9600, "type" => "fiction"],
-    ["titre" => "Découverte de l'Amérique", "date" => 1492, "type" => "reel"],
-    ["titre" => "Lancement de Skynet", "date" => 1997, "type" => "fiction"],
-    ["titre" => "Révolution Française", "date" => 1789, "type" => "reel"]
+    ["titre" => "Construction de Gizeh", "date" => -2560],
+    ["titre" => "Sacre de Charlemagne", "date" => 800],
+    ["titre" => "Premier pas sur la Lune", "date" => 1969],
+    ["titre" => "Chute de l'Empire Romain", "date" => 476],
+    ["titre" => "Découverte de l'Amérique", "date" => 1492],
+    ["titre" => "Révolution Française", "date" => 1789],
+    ["titre" => "Invention de l'imprimerie", "date" => 1440],
+    ["titre" => "Chute du mur de Berlin", "date" => 1989]
 ];
 
 echo json_encode($evenements[array_rand($evenements)]);
