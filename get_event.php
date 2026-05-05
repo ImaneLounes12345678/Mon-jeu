6716875892140982f470cc1a9143d5bc28d0d78d
ImaneLounes12345678 <imanelounes351@gmail.com>
2026-05-05 22:29:29 +0200

    Refactor historical events in get_event.php

--- get_event.php
+++ get_event.php
@@ -2,14 +2,21 @@
 header('Content-Type: application/json');
 
 $evenements = [
-    ["titre" => "Construction de Gizeh", "date" => -2560],
-    ["titre" => "Sacre de Charlemagne", "date" => 800],
+    ["titre" => "Gizeh", "date" => -2560],
+    ["titre" => "Charlemagne", "date" => 800],
     ["titre" => "Premier pas sur la Lune", "date" => 1969],
-    ["titre" => "Chute de l'Empire Romain", "date" => 476],
+    ["titre" => "Chute de Rome", "date" => 476],
     ["titre" => "Découverte de l'Amérique", "date" => 1492],
     ["titre" => "Révolution Française", "date" => 1789],
-    ["titre" => "Invention de l'imprimerie", "date" => 1440],
-    ["titre" => "Chute du mur de Berlin", "date" => 1989]
+    ["titre" => "L'Imprimerie", "date" => 1440],
+    ["titre" => "Chute du Mur de Berlin", "date" => 1989],
+    ["titre" => "Code de Hammurabi", "date" => -1750],
+    ["titre" => "Bataille de Marignan", "date" => 1515],
+    ["titre" => "Déclaration d'Indépendance USA", "date" => 1776],
+    ["titre" => "Prise de la Bastille", "date" => 1789],
+    ["titre" => "Fin de l'Apartheid", "date" => 1994],
+    ["titre" => "Invention du Cinéma", "date" => 1895],
+    ["titre" => "Chute de Constantinople", "date" => 1453]
 ];
 
 echo json_encode($evenements[array_rand($evenements)]);
